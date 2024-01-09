edace16c97c142a5fa854898146fb76350fdfb66
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-01-09 04:16:57 +0400

    Added support for multiple auth strategies

--- client/client.go
+++ client/client.go
@@ -10,12 +10,31 @@ type Client struct {
 	Config Config
 }
 
+type AuthStrategyType string
+
+var AuthStrategy = struct {
+	SERVICE_TOKEN              AuthStrategyType
+	UNIVERSAL_MACHINE_IDENTITY AuthStrategyType
+}{
+	SERVICE_TOKEN:              "SERVICE_TOKEN",
+	UNIVERSAL_MACHINE_IDENTITY: "UNIVERSAL_MACHINE_IDENTITY",
+}
+
 type Config struct {
-	HostURL      string
+	HostURL string
+
+	AuthStrategy AuthStrategyType
+
+	// Service Token Auth
 	ServiceToken string
-	EnvSlug      string
-	SecretsPath  string
-	HttpClient   *resty.Client // By default a client will be created
+
+	// Universal Machine Identity Auth
+	ClientId     string
+	ClientSecret string
+
+	EnvSlug     string
+	SecretsPath string
+	HttpClient  *resty.Client // By default a client will be created
 }
 
 func NewClient(cnf Config) (*Client, error) {
@@ -24,14 +43,32 @@ func NewClient(cnf Config) (*Client, error) {
 		cnf.HttpClient.SetBaseURL(cnf.HostURL)
 	}
 
-	if cnf.ServiceToken == "" {
-		return nil, fmt.Errorf("you must set the service token for the client before making calls")
+	if cnf.ServiceToken == "" && cnf.ClientId == "" && cnf.ClientSecret == "" {
+		return nil, fmt.Errorf("you must set the service token, or a client secret and client ID for the client before making calls")
 	}
 
-	if cnf.ServiceToken != "" {
-		cnf.HttpClient.SetAuthToken(cnf.ServiceToken)
+	var authToken string
+
+	if cnf.ClientId != "" && cnf.ClientSecret != "" {
+		token, err := Client{cnf}.UniversalMachineIdentityAuth()
+
+		if err != nil {
+			return nil, fmt.Errorf("unable to authenticate with universal machine identity [err=%s]", err)
+		}
+
+		authToken = token
+		cnf.AuthStrategy = AuthStrategy.UNIVERSAL_MACHINE_IDENTITY
+	}
+	if cnf.ServiceToken != "" && authToken == "" {
+		authToken = cnf.ServiceToken
+		cnf.AuthStrategy = AuthStrategy.SERVICE_TOKEN
 	}
 
+	if authToken != "" {
+		cnf.HttpClient.SetAuthToken(authToken)
+	} else {
+		return nil, fmt.Errorf("no authentication credentials provided. You must define the service_token, or client_id and client_secret field of the provider")
+	}
 	if cnf.EnvSlug != "" {
 		return nil, fmt.Errorf("you must set the environment before making calls")
 	}
