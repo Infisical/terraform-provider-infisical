9a6e818ea1335fe57b32e6ccaac1598e3aa9d5c4
swapnilkuwar <swapnilkuwar1995@gmail.com>
2024-08-01 08:04:38 +0530

    Infisical import secret resource added.

--- internal/client/helpers.go
+++ internal/client/helpers.go
@@ -14,6 +14,12 @@ type DecodedSymmetricEncryptionDetails = struct {
 	Key    []byte
 }
 
+// Define a generic not found error type
+type NotFoundError struct {
+	Resource string
+	ID       string
+}
+
 func GetBase64DecodedSymmetricEncryptionDetails(key string, cipher string, IV string, tag string) (DecodedSymmetricEncryptionDetails, error) {
 	cipherx, err := base64.StdEncoding.DecodeString(cipher)
 	if err != nil {
@@ -50,4 +56,16 @@ func GetSymmetricKeyFromServiceToken(serviceToken string) (privateKey string, er
 	}
 
 	return serviceTokenParts[3], nil
+}
+
+func (e *NotFoundError) Error() string {
+	return fmt.Sprintf("%s with ID %s not found", e.Resource, e.ID)
+}
+
+// A helper function to create a new NotFoundError
+func NewNotFoundError(resource, id string) error {
+	return &NotFoundError{
+		Resource: resource,
+		ID:       id,
+	}
 }
