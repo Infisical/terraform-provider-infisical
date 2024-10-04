6a259a743b70859417fd95457adb8470846c4718
Daniel Hougaard <daniel@infisical.com>
2024-10-05 01:54:23 +0400

    docs(aws-integrations): auto-generated documentation

---
+++ examples/resources/infisical_integration_aws_parameter_store/resource.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    infisical = {
+      # version = <latest version>
+      source = "infisical/infisical"
+    }
+  }
+}
+
+provider "infisical" {
+  host          = "https://app.infisical.com" # Only required if using self hosted instance of Infisical, default is https://app.infisical.com
+  client_id     = "<machine-identity-client-id>"
+  client_secret = "<machine-identity-client-secret>"
+}
+
+
+resource "infisical_integration_aws_parameter_store" "parameter-store-integration" {
+  project_id  = "<project-id>"
+  environment = "<env-slug>" // example, dev
+
+  secret_path          = "<infisical-secrets-path>" // example, /folder, or /
+  parameter_store_path = "/example/secrets"
+
+  aws_region        = "<aws-region>" // example, us-east-2
+  access_key_id     = "<aws-access-key-id>"
+  secret_access_key = "<aws-secret-access-key>"
+
+  // Optional
+  options = {
+    should_disable_delete = true // Optional, default is false
+    aws_tags = [                 // Optional
+      {
+        key   = "key",
+        value = "value"
+      },
+    ]
+  }
+}
