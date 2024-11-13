9ff3fdb13c39a31fc76d802f1fe3495f2a66c553
= <akhilmhdh@gmail.com>
2024-11-14 00:35:13 +0530

    feat: completed tf testing base

--- go.mod
+++ go.mod
@@ -6,25 +6,27 @@ toolchain go1.23.1
 
 require (
 	github.com/go-resty/resty/v2 v2.7.0
-	github.com/hashicorp-demoapp/hashicups-client-go v0.1.0
 	github.com/hashicorp/terraform-plugin-docs v0.15.0
 	github.com/hashicorp/terraform-plugin-framework v1.10.0
+	github.com/hashicorp/terraform-plugin-framework-validators v0.13.0
 	github.com/hashicorp/terraform-plugin-go v0.23.0
 	github.com/hashicorp/terraform-plugin-log v0.9.0
-	github.com/hashicorp/terraform-plugin-testing v1.2.0
+	github.com/hashicorp/terraform-plugin-testing v1.10.0
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.26.0
 )
 
 require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
-	github.com/Masterminds/semver/v3 v3.1.1 // indirect
-	github.com/Masterminds/sprig/v3 v3.2.2 // indirect
-	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
+	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
+	github.com/ProtonMail/go-crypto v1.1.0-alpha.2 // indirect
 	github.com/agext/levenshtein v1.2.2 // indirect
-	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/bgentry/speakeasy v0.1.0 // indirect
-	github.com/cloudflare/circl v1.3.3 // indirect
-	github.com/fatih/color v1.15.0 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -32,25 +34,25 @@ require (
 	github.com/hashicorp/go-checkpoint v0.5.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320 // indirect
-	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-hclog v1.6.3 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-plugin v1.6.0 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
-	github.com/hashicorp/go-version v1.6.0 // indirect
-	github.com/hashicorp/hc-install v0.5.2 // indirect
-	github.com/hashicorp/hcl/v2 v2.16.2 // indirect
+	github.com/hashicorp/go-version v1.7.0 // indirect
+	github.com/hashicorp/hc-install v0.8.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.21.0 // indirect
 	github.com/hashicorp/logutils v1.0.0 // indirect
-	github.com/hashicorp/terraform-exec v0.18.1 // indirect
-	github.com/hashicorp/terraform-json v0.16.0 // indirect
-	github.com/hashicorp/terraform-plugin-framework-validators v0.13.0 // indirect
-	github.com/hashicorp/terraform-plugin-sdk/v2 v2.26.1 // indirect
+	github.com/hashicorp/terraform-exec v0.21.0 // indirect
+	github.com/hashicorp/terraform-json v0.22.1 // indirect
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.34.0 // indirect
 	github.com/hashicorp/terraform-registry-address v0.2.3 // indirect
 	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
-	github.com/huandu/xstrings v1.3.2 // indirect
-	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/huandu/xstrings v1.3.3 // indirect
+	github.com/imdario/mergo v0.3.15 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/cli v1.1.5 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
@@ -65,12 +67,13 @@ require (
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	github.com/zclconf/go-cty v1.13.2 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/zclconf/go-cty v1.15.0 // indirect
+	golang.org/x/mod v0.19.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
 	google.golang.org/grpc v1.63.2 // indirect
---
+++ internal/testacc/project_resource_test.go
@@ -0,0 +1,135 @@
+package testAcc
+
+import (
+	infisicalclient "terraform-provider-infisical/internal/client"
+	"testing"
+
+	tfjson "github.com/hashicorp/terraform-json"
+	"github.com/hashicorp/terraform-plugin-testing/compare"
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccProject(t *testing.T) {
+	idValueCompare := statecheck.CompareValue(compare.ValuesSame())
+	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	changedRandomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+
+	slug := "test-project"
+
+	projectResource := "infisical_project.test_project"
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: protoV6ProviderFactories(),
+		PreCheck:                 preCheck(t),
+		Steps: []resource.TestStep{
+			{
+				ConfigFile: config.StaticFile("./testdata/project_resource/project_1.tf"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectNonEmptyPlan(),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					idValueCompare.AddStateValue(
+						projectResource,
+						tfjsonpath.New("id"),
+					),
+					statecheck.ExpectKnownValue(projectResource, tfjsonpath.New("name"), knownvalue.StringExact(randomName)),
+					statecheck.ExpectKnownValue(projectResource, tfjsonpath.New("slug"), knownvalue.StringExact(slug)),
+					// check in infisical
+					ExpectExternalResource(projectResource,
+						func(resource *tfjson.StateResource) error {
+							id, err := tfjsonpath.Traverse(resource.AttributeValues, tfjsonpath.New("id"))
+							if err != nil {
+								return err
+							}
+							projectDetail, err := infisicalApiClient.GetProjectById(infisicalclient.GetProjectByIdRequest{
+								ID: id.(string),
+							})
+
+							slug, err := tfjsonpath.Traverse(resource.AttributeValues, tfjsonpath.New("slug"))
+							if err != nil {
+								return err
+							}
+							if err := knownvalue.StringExact(projectDetail.Slug).CheckValue(slug); err != nil {
+								return err
+							}
+							return nil
+						},
+					),
+				},
+				ConfigVariables: config.Variables{
+					"project_name": config.StringVariable(randomName),
+					"project_slug": config.StringVariable(slug),
+				},
+			},
+			// check for drift
+			{
+				ConfigFile: config.StaticFile("./testdata/project_resource/project_1.tf"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectEmptyPlan(),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					idValueCompare.AddStateValue(
+						projectResource,
+						tfjsonpath.New("id"),
+					),
+					statecheck.ExpectKnownValue(projectResource, tfjsonpath.New("name"), knownvalue.StringExact(randomName)),
+					statecheck.ExpectKnownValue(projectResource, tfjsonpath.New("slug"), knownvalue.StringExact(slug)),
+				},
+				ConfigVariables: config.Variables{
+					"project_name": config.StringVariable(randomName),
+					"project_slug": config.StringVariable(slug),
+				},
+			},
+			{
+				ConfigFile: config.StaticFile("./testdata/project_resource/project_1.tf"),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectNonEmptyPlan(),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					idValueCompare.AddStateValue(
+						projectResource,
+						tfjsonpath.New("id"),
+					),
+					statecheck.ExpectKnownValue(projectResource, tfjsonpath.New("name"), knownvalue.StringExact(changedRandomName)),
+					statecheck.ExpectKnownValue(projectResource, tfjsonpath.New("slug"), knownvalue.StringExact(slug)),
+					// check in infisical
+					ExpectExternalResource(projectResource,
+						func(resource *tfjson.StateResource) error {
+							id, err := tfjsonpath.Traverse(resource.AttributeValues, tfjsonpath.New("id"))
+							if err != nil {
+								return err
+							}
+							projectDetail, err := infisicalApiClient.GetProjectById(infisicalclient.GetProjectByIdRequest{
+								ID: id.(string),
+							})
+							slug, err := tfjsonpath.Traverse(resource.AttributeValues, tfjsonpath.New("slug"))
+							if err != nil {
+								return err
+							}
+							if err := knownvalue.StringExact(projectDetail.Slug).CheckValue(slug); err != nil {
+								return err
+							}
+							return nil
+						},
+					),
+				},
+				ConfigVariables: config.Variables{
+					"project_name": config.StringVariable(changedRandomName),
+					"project_slug": config.StringVariable(slug),
+				},
+			},
+		},
+	})
+}
