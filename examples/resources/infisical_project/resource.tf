ab362361b2640672879a7c8cb050e604693c862d
x032205 <x032205@gmail.com>
2025-05-09 20:41:27 -0400

    review fixes

--- examples/resources/infisical_project/resource.tf
+++ examples/resources/infisical_project/resource.tf
@@ -18,17 +18,15 @@ provider "infisical" {
 }
 
 resource "infisical_project" "gcp-project" {
-  name                  = "GCP Project"
-  slug                  = "gcp-project"
-  description           = "This is a GCP project"
-  has_delete_protection = true
+  name        = "GCP Project"
+  slug        = "gcp-project"
+  description = "This is a GCP project"
 }
 
 resource "infisical_project" "aws-project" {
-  name                  = "AWS Project"
-  slug                  = "aws-project"
-  description           = "This is an AWS project"
-  has_delete_protection = false
+  name        = "AWS Project"
+  slug        = "aws-project"
+  description = "This is an AWS project"
 }
 
 resource "infisical_project" "azure-project" {
