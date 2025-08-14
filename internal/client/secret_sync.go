c605b062ae1096db37cca2a1f1593bdac0055965
sidwebworks <58144379+sidwebworks@users.noreply.github.com>
2025-08-15 02:27:04 +0530

    feat: cloudflare secrets sync resource

--- internal/client/secret_sync.go
+++ internal/client/secret_sync.go
@@ -20,6 +20,7 @@ const (
 	SecretSyncAppRender                SecretSyncApp = "render"
 	SecretSyncAppBitbucket             SecretSyncApp = "bitbucket"
 	SecretSyncAppDatabricks            SecretSyncApp = "databricks"
+	SecretSyncAppCloudflareWorkers     SecretSyncApp = "cloudflare-workers"
 )
 
 type SecretSyncBehavior string
