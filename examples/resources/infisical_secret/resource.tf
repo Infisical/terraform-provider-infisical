d0337b130bb1e72997f595aff7c94d0a107b59db
Daniel Hougaard <daniel@infisical.com>
2025-09-02 18:18:21 +0200

    docs

--- examples/resources/infisical_secret/resource.tf
+++ examples/resources/infisical_secret/resource.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     infisical = {
       # version = <latest version>
-      source = "hashicorp.com/edu/infisical"
+      source = "infisical/infisical"
     }
   }
 }
@@ -11,16 +11,54 @@ provider "infisical" {
   host = "https://app.infisical.com" # Only required if using self hosted instance of Infisical, default is https://app.infisical.com
   auth = {
     universal = {
-      client_id     = "b84dd7c6-a773-4870-9da3-e49192844c6c"
-      client_secret = "8e40851114c90312f970bdca9453fe5a0c404c5017a6025977654b62eabaf322"
+      client_id     = "<machine-identity-client-id>"
+      client_secret = "<machine-identity-client-secret>"
     }
   }
 }
 
 resource "infisical_secret" "mongo_secret" {
-  name         = "MONGO_DB-new"
-  value        = "<some-key>2"
+  name         = "MONGO_DB"
+  value        = "<some-key>"
   env_slug     = "dev"
-  workspace_id = "5156a345-e460-416b-84fc-b14b426b1cb3"
+  workspace_id = "PROJECT_ID"
+  folder_path  = "/"
+}
+
+resource "infisical_secret" "smtp_secret" {
+  name         = "SMTP"
+  value        = "<some key>"
+  env_slug     = "dev"
+  workspace_id = "PROJECT_ID"
+  folder_path  = "/mail-service"
+  secret_reminder = {
+    note        = "Rotate this secret using X API"
+    repeat_days = 30
+  }
+}
+
+
+resource "infisical_secret_tag" "terraform" {
+  name       = "terraform"
+  slug       = "terraform"
+  color      = "#fff"
+  project_id = "PROJECT_ID"
+}
+
+resource "infisical_secret" "github_action_secret" {
+  name         = "GITHUB_ACTION"
+  value        = "<some value>"
+  env_slug     = "dev"
+  workspace_id = "PROJECT_ID"
+  folder_path  = "/"
+  tag_ids      = [infisical_secret_tag.terraform.id]
+}
+
+# Ephemeral resource (requires Terraform 1.10.0+)
+# https://www.hashicorp.com/blog/terraform-1-10-improves-handling-secrets-in-state-with-ephemeral-values
+ephemeral "infisical_secret" "ephemeral-secret" {
+  name         = "SECRET-KEY"
+  env_slug     = "dev"
+  workspace_id = "PROJECT_ID"
   folder_path  = "/"
 }
