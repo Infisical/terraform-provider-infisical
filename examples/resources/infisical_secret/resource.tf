0163737e0a487e6acca9376fe602ced8e7808af9
Daniel Hougaard <daniel@infisical.com>
2025-09-24 03:21:11 +0400

    fix: infinate drift on

--- examples/resources/infisical_secret/resource.tf
+++ examples/resources/infisical_secret/resource.tf
@@ -2,63 +2,36 @@ terraform {
   required_providers {
     infisical = {
       # version = <latest version>
-      source = "infisical/infisical"
+      source = "hashicorp.com/edu/infisical"
     }
   }
 }
 
 provider "infisical" {
-  host = "https://app.infisical.com" # Only required if using self hosted instance of Infisical, default is https://app.infisical.com
+  host = "http://localhost:8080" # Only required if using self hosted instance of Infisical, default is https://app.infisical.com
   auth = {
     universal = {
-      client_id     = "<machine-identity-client-id>"
-      client_secret = "<machine-identity-client-secret>"
+      client_id     = "0627787b-63bb-45d2-90c7-7e47cbb68dc6"
+      client_secret = "91e1e75064d8d9982b5ae141f9eb0dbe7c200e065fce3c2783fb2d9e82e47efb"
     }
   }
 }
 
-resource "infisical_secret" "mongo_secret" {
-  name         = "MONGO_DB"
-  value        = "<some-key>"
-  env_slug     = "dev"
-  workspace_id = "PROJECT_ID"
+resource "infisical_secret" "write-only-secret" {
+  name             = "WRITE-ONLY-SECRET"
+  env_slug         = "prod-bk03"
+  value_wo         = "test-valuetestddsssssaaadddssaa"
+  value_wo_version = 1
+  workspace_id     = "cc7c320b-b0c1-422f-831a-a51d536dc3c2"
+  folder_path      = "/"
+}
+
+resource "infisical_secret" "normal-secret" {
+  name         = "NORMAL-SECRET"
+  env_slug     = "prod-bk03"
+  value        = "test-valuetestddsssssaaadddssaa"
+  workspace_id = "cc7c320b-b0c1-422f-831a-a51d536dc3c2"
   folder_path  = "/"
 }
 
-resource "infisical_secret" "smtp_secret" {
-  name         = "SMTP"
-  value        = "<some key>"
-  env_slug     = "dev"
-  workspace_id = "PROJECT_ID"
-  folder_path  = "/mail-service"
-  secret_reminder = {
-    note        = "Rotate this secret using X API"
-    repeat_days = 30
-  }
-}
 
-
-resource "infisical_secret_tag" "terraform" {
-  name       = "terraform"
-  slug       = "terraform"
-  color      = "#fff"
-  project_id = "PROJECT_ID"
-}
-
-resource "infisical_secret" "github_action_secret" {
-  name         = "GITHUB_ACTION"
-  value        = "<some value>"
-  env_slug     = "dev"
-  workspace_id = "PROJECT_ID"
-  folder_path  = "/"
-  tag_ids      = [infisical_secret_tag.terraform.id]
-}
-
-# Ephemeral resource (requires Terraform 1.10.0+)
-# https://www.hashicorp.com/blog/terraform-1-10-improves-handling-secrets-in-state-with-ephemeral-values
-ephemeral "infisical_secret" "ephemeral-secret" {
-  name         = "SECRET-KEY"
-  env_slug     = "dev"
-  workspace_id = "PROJECT_ID"
-  folder_path  = "/"
-}
